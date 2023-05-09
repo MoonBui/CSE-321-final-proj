c1a06b383dbd19f4c0bfd9e2562a5c961f17b5bb
Jenn Pham <91994299+phamav@users.noreply.github.com>
2023-05-08 23:30:44 -0400

    Update YahtzeeEngine.java

--- YahtzeeEngine.java
+++ YahtzeeEngine.java
@@ -1,291 +1,195 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 /**
- * Code for a Yahtzee's logic implementation
+ * Yahtzee Console for GUI Implementation
+ * Needs to be changed.
+ * Printing to console for now.
  */
-public class YahtzeeEngine {
-	// private variables
-	public Dice[] dice; // the array of the rolled dice
-	public int round, bonusPoint; // 100 bonus for each Yahtzee after the first one
-	public int[] upper, lower; // arrays for upper and lower boards; use for entering scores.
-	public boolean[] scoredUpper, scoredLower; // arrays for scored upper/lower sections
+import java.util.Arrays;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class YahtzeeGUI {
+	// Private variables
+	private YahtzeeEngine game;
+	private String[] names = { "Aces", "Twos", "Threes", "Fours", "Fives", "Sixes", "Three of a Kind", "Four of a Kind",
+			"Full House", "Small Straight", "Large Straight", "Yahtzee", "Chance" };
+	private JFrame frame;
+	private JTextArea ta;
+	private JTextField tf;
+	private String input;
 	
-	// Final variables
-	private final int MAX_ROUNDS = 13; // number of rounds in a game
-	
-	/**
-	 * Constructor YahtzeeEngine
-	 */
-	public YahtzeeEngine() {
-		// roll the Dice
-		resetDice();
-		
-		// Reset the variables for a new Game
-		upper = new int[6];
-		lower = new int[7];
-		scoredUpper = new boolean[6];
-		scoredLower = new boolean[7];
-		round = bonusPoint = 0;
-	}
 
 	/**
-	 * Roll method for each round.
-	 * Take in a boolean array to check for which die is rerolled.
-	 * 
-	 * @param reroll
+	 * Constructor for GUI
 	 */
-	public void rerollDice(boolean[] reroll) {
-		for (int i = 0; i < 5; i++) {
-			if (reroll[i]) {
-				dice[i] = new Dice();
-			}
-		}
-	}
-
-	/**
-	 * Check if the category can be scored. True if empty, else false.
-	 * @param category
-	 * @return boolean
-	 */
-	public boolean scorable(int category) {		
-        if (getSection(category) == 1) {
-        	if (!scoredUpper[category]) return true;
-        } else if (getSection(category) == 2) {
-        	if (!scoredLower[category - 6]) return true;
-        }
+	public YahtzeeGUI() {
+		game = new YahtzeeEngine();
+		frame = new JFrame("Yahtzee Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,800);
+        JLabel label = new JLabel("Enter Text");
+        JButton send = new JButton("Send");
+        ta = new JTextArea();
+        tf = new JTextField(10); // accepts up to 10 characters
         
-        return false;
-	}
-	
-	/**
-	 * update category with score if it's scorable.
-	 * updates the bonus Yahtzee counter if needed.
-	 *
-	 * @param category the index of the category to score.
-	 */
-	public void score(int category) {
-		int score = 0;
-		if (scorable(category)) {
-			score = computeScore(category);
-			if (getSection(category) == 1) {
-				upper[category] = score;
-				scoredUpper[category] = true;
-			} else {
-				lower[category - 6] = score;
-				scoredLower[category - 6] = true;
+        //
+        JPanel panel = new JPanel(); // the panel is not visible in output
+        panel.add(label); // Components Added using Flow Layout
+        panel.add(tf);
+        panel.add(send);
+        
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.setVisible(true);
+        
+        send.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				input = tf.getText(); 
 			}
-			round++;
+        });
+        
+	}
+
+	/**
+	 * Main play function to initiate game
+	 */
+	public void play() {
+		while (!game.isOver()) {
+			playARound();
+		}
+		showTotal();
+		
+		waitInput();
+		String playOption = input;
+
+		if (playOption.equalsIgnoreCase("y")) {
+			input = null;
+			game = new YahtzeeEngine();
+			play();
 		}
 	}
 
 	/**
-	 * Calculate Score for a category
-	 * @param category
-	 * @return
+	 * Function to excute rounds of the game
 	 */
-	public int computeScore(int category) {
-		// 0 1 2 3 4 5 = 1 2 3 4 5 6
-		int[] countForEachNum = new int[6]; // number of counts for each face value of a die
+	private void playARound() {
+		game.resetDice();
+		ta.setText("Round " + (game.getRound() + 1) + ":\n");
+		boolean[] reroll = new boolean[5];
 		
-		for (Dice d : dice) {
-			countForEachNum[d.getVal() - 1]++;
-		}
 
-		if (getSection(category) == 1) {
-			return countForEachNum[category] * (category + 1);
-		} else {
-			switch (category) {
-			case 6: // three of a kind
-				for (int count : countForEachNum) {
-					if (count >= 3) return getDiceSum(dice);
-				}
-				break;
-			case 7: // four of a kind
-				for (int count : countForEachNum) {
-					if (count >= 4) return getDiceSum(dice);
-				}
-				break;
-			case 8: // full house
-				boolean threeOfAKind = false;
-				boolean pair = false;
-				for (int count : countForEachNum) {
-					if (count == 3) threeOfAKind = true;
-					else if (count == 2) pair = true;
-				}
+		for (int roll = 0; roll < 3; roll++) {
+			game.rerollDice(reroll);
+			ta.append(showDice());
+
+			if (roll < 2) {
+				ta.append("Enter the dice to reroll (1-5) with no spaces, or 0 to keep all. "
+						+ "\nFor example, if you want to reroll the 2nd and 3rd die, enter 23: \n");
+
+				// Wait for user input before proceeding
+				waitInput();			
+				String rerollInput = input;
 				
-				if (pair && threeOfAKind) return 25;
+				if (Integer.parseInt(input) != 0) {
+	                for (int i = 0; i < 5; i++) {
+	                    reroll[i] = rerollInput.contains(String.valueOf(i + 1));
+	                }
+				} else 
+					roll = 2;
 				
-				break;
-			case 9: // small straight
-				if (isStraight(dice) == 's') return 30;
-				break;
-			case 10: // large straight
-				if (isStraight(dice) == 'l') return 40;
-                break;
-			case 11: // Yahtzee
-				for (int count : countForEachNum) {
-					if (count == 5) {
-						bonusPoint++; // update Yahtzee count
-						return 50;
-					}
-				}
-				break;
-			case 12: // Chance
-				return getDiceSum(dice);
-			default:
-				break;
+                input = null;
 			}
 		}
 		
-		return 0;
+		ta.append("Scoring board: \n");
+		showScoreOption();
+		
+		int category;
+		do {
+			ta.append("Enter only ONE category to score (0-12): \n");
+			waitInput();
+			category = Integer.parseInt(input);
+			input = null;
+		} while (!game.scorable(category));
+			game.score(category);
+			
 	}
-	
+
 	/**
-	 * Get total score for the game.
-	 * 
-	 * @return int total score.
+	 * Function to grab dice rolls
+	 * @return a total of 5 dice rolls
 	 */
-	public int getTotalScore() {
-		// Upper deck sum	    
-	    int upperSum = 0;
-	    for (int i = 0; i < upper.length; i++) {
-	        upperSum += upper[i];
-	    }
-	    
-	    // if the user has over 63 points in the upper section, then they receive a bonus of 35 points.
-        if (upperSum >= 63) upperSum += 35;
-        
-        // Lower deck sum
-	    int lowerSum = 0;
-	    for (int i = 0; i < lower.length; i++) {
-	        lowerSum += lower[i];
-	    }
-	    
-	    // add Yahtzee bonus
-        if (bonusPoint > 0) lowerSum += (bonusPoint - 1) * 100;
-        
-	    return upperSum + lowerSum;
+	public String showDice() {
+		String result = "Roll: ";
+		for (Dice d : game.getDice()) {
+			result += d.getVal() + " ";
+		}
+
+		return result += "\n";
+		
 	}
-	
-	/*****HELPER METHODS****/
+
 	/**
-	 * Get the section
+	 * Function to show score
+	 */
+	public void showScoreOption() {
+		String result = "";
+		for (int i = 0; i < 13; i++) {
+			if (game.scorable(i)) {
+				result += i + ". " + getCategory(i) + ": " + game.computeScore(i) + "\n";
+			}
+		}
+		
+		ta.append(result);
+	}
+
+	/**
+	 * Function to show score board
+	 */
+	public void showTotal() {
+	    String upper = "";
+	    String lower = "";
+
+	    ta.append("FINAL SCORE BOARD: \n");
+	    for (int i = 0; i < 6; i++) {
+	        upper += i + ". " + getCategory(i) + ": " + game.getUpper()[i] + "\n";
+	    }
+	    ta.append(upper);
+
+	    for (int i = 6; i < 13; i++) {
+	        lower += i + ". " + getCategory(i) + ": " + game.getLower()[i - 6] + "\n";
+	    }
+	    ta.append(lower);
+	    ta.append("Total score: " + game.getTotalScore() + "\n"); 
+		ta.append("Play another game? (y/n): ");
+	}
+
+	/****PRIVATE METHODS***/
+	
+	/**
+	 * 
 	 * @param category
 	 * @return
 	 */
-	private int getSection(int category) {
-		if (category >= 0 && category < 6) return 1;
-		else if (category >= 6 && category < 13) return 2;
-		
-		return 0;
-	}
-	/**
-	 * Get total sum of Dice array
-	 * @param dice
-	 * @return
-	 */
-	private int getDiceSum(Dice[] dice) {
-		int result = 0;
-		for (Dice d : dice) {
-	        result += d.getVal();
-	    }
-		return result;
+	private String getCategory(int category) {
+		return names[category];
 	}
 	
 	/**
-	 * Count consecutive values in a dice array
-	 * @param counts
-	 * @return
+	 * Helper function to sleep a thread until input value is not null
 	 */
-	private char isStraight(Dice[] d) {
-		// sort the dice array
-		Arrays.sort(d, new Comparator<Dice>() {
-	        @Override
-	        public int compare(Dice d1, Dice d2) {
-	            return d1.getVal() - d2.getVal();
+	private void waitInput() {
+		while (input == null) {
+	        try {
+	            Thread.sleep(100);
+	        } catch (InterruptedException e) {
+	            // ignore
 	        }
-	    });
-
-		// variables for straight scenarios
-		// l1, l2 is large straight
-		// s1, s2, s3 is small straight
-		boolean l1 = (d[0].getVal() == 1) && (d[1].getVal() == 2) && (d[2].getVal() == 3) && (d[3].getVal() == 4) && (d[4].getVal() == 5);
-		
-		boolean l2 = (d[0].getVal() == 2) && (d[1].getVal() == 3) && (d[2].getVal() == 4) && (d[3].getVal() == 5) && (d[4].getVal() == 6);
-		
-		boolean s1 = (d[0].getVal() == 1) && (d[1].getVal() == 2) && (d[2].getVal() == 3) && (d[3].getVal() == 4);
-		
-		boolean s2 = (d[0].getVal() == 2) && (d[1].getVal() == 3) && (d[2].getVal() == 4) && (d[3].getVal() == 5);
-		
-		boolean s3 = (d[0].getVal() == 3) && (d[1].getVal() == 4) && (d[2].getVal() == 5) && (d[3].getVal() == 6);
-		
-		// return
-		if (l1 || l2) {
-	        return 'l';
-	    } else if (s1 || s2 || s3) {
-	        return 's';   // Small straight
 	    }
-		
-	    return 'n';
-		
 	}
 	
-	/**
-	 * Reset the dice for each round (not reroll).
-	 */
-	public void resetDice() {
-		dice = new Dice[5];
-        for (int i = 0; i < 5; i++) {
-            dice[i] = new Dice();
-        }
-	}
-	
-	/**
-	 * Get Dice Array
-	 * @return Dice[]
-	 */
-	public Dice[] getDice() {
-		return dice;
-	}
-
-	/**
-	 * Check if the game is over.
-	 * @return boolean
-	 */
-	public boolean isOver() {
-		return round >= MAX_ROUNDS;
-	}
-	
-	/**
-	 * Get Upper Section Potential Score
-	 * @return int[]
-	 */
-	public int[] getUpper() {
-		return upper;
-	}
-
-	/**
-	 * Get Lower Section Potential Score
-	 * @return int[]
-	 */
-	public int[] getLower() {
-		return lower;
-	}
-
-	/**
-	 * Get the current round
-	 * @return int
-	 */
-	public int getRound() {
-		return round;
-	}
-
-	/**
-	 * Get the Bonus point
-	 * @return int
-	 */
-	public int getBonusYahtzee() {
-		return bonusPoint;
-	}
 }
